d1cc43ed853f8e52809c5689c4d750149df4730a
Christian Berndt <christian.berndt@liferay.com>
2020-01-10 11:14:28 +0100

    Initial check-in.

---
+++ src/main/java/com/chberndt/liferay/install/RolesImporter.java
@@ -0,0 +1,149 @@
+package com.chberndt.liferay.install;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.ArrayUtil;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * 
+ * @author Christian Berndt
+ * 
+ *         RolesImporter is a stripped version of
+ *         com.liferay.commerce.product.internal.importer.CPFileImporterImpl
+ *
+ */
+@Component(
+	immediate=true,
+	service=RolesImporter.class
+)
+public class RolesImporter {
+
+	public void createRoles(JSONArray jsonArray, ServiceContext serviceContext) throws PortalException {
+		
+		for (int i = 0; i < jsonArray.length(); i++) {
+			JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+			JSONObject actionsJSONObject = jsonObject.getJSONObject("actions");
+			String name = jsonObject.getString("name");
+			int scope = jsonObject.getInt("scope");
+			int type = jsonObject.getInt("type");
+
+			_log.info("Adding or updating Role " + name);
+
+			Role role = getRole(name, type, serviceContext);
+
+			updateActions(role, actionsJSONObject, scope, serviceContext);
+		}
+	}
+
+	protected Role getRole(String name, int type, ServiceContext serviceContext) throws PortalException {
+
+		Map<Locale, String> titleMap = new HashMap<>();
+
+		titleMap.put(serviceContext.getLocale(), name);
+
+		Role role = _roleLocalService.fetchRole(serviceContext.getCompanyId(), name);
+
+		if (role == null) {
+			role = _roleLocalService.addRole(serviceContext.getUserId(), null, 0, name, titleMap, null, type, null,
+					serviceContext);
+		}
+
+		return role;
+	}
+
+	protected void updateAction(Role role, String resource, String actionId, int scope, ServiceContext serviceContext)
+			throws PortalException {
+
+		if (scope == ResourceConstants.SCOPE_COMPANY) {
+			_resourcePermissionLocalService.addResourcePermission(serviceContext.getCompanyId(), resource, scope,
+					String.valueOf(role.getCompanyId()), role.getRoleId(), actionId);
+		} else if (scope == ResourceConstants.SCOPE_GROUP_TEMPLATE) {
+			_resourcePermissionLocalService.addResourcePermission(serviceContext.getCompanyId(), resource,
+					ResourceConstants.SCOPE_GROUP_TEMPLATE, String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
+					role.getRoleId(), actionId);
+		} else if (scope == ResourceConstants.SCOPE_GROUP) {
+			_resourcePermissionLocalService.removeResourcePermissions(serviceContext.getCompanyId(), resource,
+					ResourceConstants.SCOPE_GROUP, role.getRoleId(), actionId);
+
+			_resourcePermissionLocalService.addResourcePermission(serviceContext.getCompanyId(), resource,
+					ResourceConstants.SCOPE_GROUP, String.valueOf(serviceContext.getScopeGroupId()), role.getRoleId(),
+					actionId);
+		}
+	}
+
+	protected void updateActions(Role role, JSONObject jsonObject, int scope, ServiceContext serviceContext)
+			throws PortalException {
+
+		String resource = jsonObject.getString("resource");
+		JSONArray actionIdsJSONArray = jsonObject.getJSONArray("actionIds");
+
+		for (int i = 0; i < actionIdsJSONArray.length(); i++) {
+			String actionId = actionIdsJSONArray.getString(i);
+
+			updateAction(role, resource, actionId, scope, serviceContext);
+		}
+	}
+
+	protected void updatePermissions(long companyId, String name, String primKey, JSONArray jsonArray)
+			throws PortalException {
+
+		if (jsonArray == null) {
+			jsonArray = JSONFactoryUtil
+					.createJSONArray("[{\"actionIds\": [\"VIEW\"], \"roleName\": \"Site Member\"," + "\"scope\": 4}]");
+		}
+
+		for (int i = 0; i < jsonArray.length(); i++) {
+			JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+			int scope = jsonObject.getInt("scope");
+
+			String roleName = jsonObject.getString("roleName");
+
+			Role role = _roleLocalService.getRole(companyId, roleName);
+
+			String[] actionIds = new String[0];
+
+			JSONArray actionIdsJSONArray = jsonObject.getJSONArray("actionIds");
+
+			if (actionIdsJSONArray != null) {
+				for (int j = 0; j < actionIdsJSONArray.length(); j++) {
+					actionIds = ArrayUtil.append(actionIds, actionIdsJSONArray.getString(j));
+				}
+			}
+
+			_resourcePermissionLocalService.setResourcePermissions(companyId, name, scope, primKey, role.getRoleId(),
+					actionIds);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(RolesImporter.class);
+
+	@Reference
+	private JSONFactory _jsonFactory;
+
+	@Reference
+	private ResourcePermissionLocalService _resourcePermissionLocalService;
+
+	@Reference
+	private RoleLocalService _roleLocalService;
+
+}
---
+++ src/main/java/com/chberndt/liferay/install/RolesInstaller.java
@@ -0,0 +1,111 @@
+package com.chberndt.liferay.install;
+
+import com.chberndt.liferay.lifecycle.action.GlobalStartupAction;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.io.IOException;
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * 
+ * @author Christian Berndt
+ *
+ */
+@Component(
+	immediate=true,
+	service=RolesInstaller.class
+)
+public class RolesInstaller {
+	
+	@Activate
+	private void activate() throws Exception {
+		
+		_log.info("RolesInstaller.activate()");
+		
+		createRoles(getServiceContext());
+		
+	}
+	
+	protected void createRoles(ServiceContext serviceContext) throws Exception {
+		
+		_log.info("RolesInstaller.createRoles()"); 
+
+		JSONArray jsonArray = _getJSONArray("roles.json");
+
+		_rolesImporter.createRoles(jsonArray, serviceContext);
+
+	}
+	
+	protected ServiceContext getServiceContext()
+			throws PortalException {
+		
+		// This strategy is limited to single instance configurations
+		long defaultCompanyId = _portal.getDefaultCompanyId();
+		
+		User user = _userLocalService.getDefaultUser(defaultCompanyId);
+
+		Locale locale = LocaleUtil.getSiteDefault();
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setAddGroupPermissions(true);
+		serviceContext.setAddGuestPermissions(true);		
+		serviceContext.setCompanyId(defaultCompanyId);
+		serviceContext.setLanguageId(_language.getLanguageId(locale));
+		serviceContext.setScopeGroupId(defaultCompanyId);
+		serviceContext.setTimeZone(user.getTimeZone());
+		serviceContext.setUserId(user.getUserId());
+
+		return serviceContext;
+	}
+	
+	private JSONArray _getJSONArray(String name) throws Exception {
+		return _jsonFactory.createJSONArray(
+			_getJSON(name));
+	}
+	
+	private String _getJSON(String name) throws IOException {
+		return StringUtil.read(RolesInstaller.class.getClassLoader(), 
+			_DEPENDENCIES_PATH + name);
+	}
+
+	private static final String _DEPENDENCIES_PATH =
+		"com/chberndt/liferay/install/dependencies/";
+	
+	private static final Log _log = LogFactoryUtil.getLog(GlobalStartupAction.class);
+	
+	@Reference
+	private JSONFactory _jsonFactory;
+	
+	@Reference
+	private Language _language;
+	
+	@Reference
+	private Portal _portal;
+	
+	@Reference
+	private RolesImporter _rolesImporter;
+	
+	@Reference
+	private GroupLocalService _groupLocalService;
+	
+	@Reference
+	private UserLocalService _userLocalService;
+
+}
